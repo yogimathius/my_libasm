64e14b2ff6d75c3940e26d95c18222507cc5bd93
rebecca silverstein <silverst_r@qwasar.io>
2024-01-15 22:49:20 +0000

    chore: adjustments for receiving args from .c

--- my_libasm.h
+++ my_libasm.h
@@ -4,16 +4,16 @@
 #include <stddef.h>
 #include <unistd.h>
 
-size_t my_strlen(const char *str);
-char *index(const char *s, int c);
-void *memcpy(void *dest, const void *src, size_t n);
-void *memmove(void *dest, const void *src, size_t n);
-void *memset(void *s, int c, size_t n);
-ssize_t read(int fd, void *buf, size_t count);
-int strcasecmp(const char *s1, const char *s2);
-size_t my_strchr(const char *str_1, int c);
-int my_strcmp(const char *str_1, const char *str_2);
-int strncmp(const char *s1, const char *s2, size_t n);
-ssize_t write(int fd, const void *buf, size_t count);
+extern size_t my_strlen(const char *str);
+extern char *index(const char *s, int c);
+extern void *memcpy(void *dest, const void *src, size_t n);
+extern void *memmove(void *dest, const void *src, size_t n);
+extern void *memset(void *s, int c, size_t n);
+extern ssize_t read(int fd, void *buf, size_t count);
+extern int strcasecmp(const char *s1, const char *s2);
+extern size_t my_strchr(const char *str_1, int c);
+extern int my_strcmp(const char *str_1, const char *str_2);
+extern int strncmp(const char *s1, const char *s2, size_t n);
+extern ssize_t write(int fd, const void *buf, size_t count);
 
 #endif  // MY_LIBASM_H

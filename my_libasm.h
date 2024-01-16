dadf4e8b657340237bd92ea29c779add2d35fdd9
rebecca silverstein <silverst_r@qwasar.io>
2024-01-16 00:42:23 +0000

    chore: refactor

--- my_libasm.h
+++ my_libasm.h
@@ -5,15 +5,15 @@
 #include <unistd.h>
 
 extern size_t my_strlen(const char *str);
-extern char *index(const char *s, int c);
-extern void *memcpy(void *dest, const void *src, size_t n);
-extern void *memmove(void *dest, const void *src, size_t n);
-extern void *memset(void *s, int c, size_t n);
-extern ssize_t read(int fd, void *buf, size_t count);
-extern int strcasecmp(const char *s1, const char *s2);
+extern char *my_index(const char *s, int c);
+extern void *my_memcpy(void *dest, const void *src, size_t n);
+extern void *my_memmove(void *dest, const void *src, size_t n);
+extern void *my_memset(void *s, int c, size_t n);
+extern ssize_t my_read(int fd, void *buf, size_t count);
+extern int my_strcasecmp(const char *s1, const char *s2);
 extern char *my_strchr(const char *str_1, int c);
 extern int my_strcmp(const char *str_1, const char *str_2);
-extern int strncmp(const char *s1, const char *s2, size_t n);
-extern ssize_t write(int fd, const void *buf, size_t count);
+extern int my_strncmp(const char *s1, const char *s2, size_t n);
+extern ssize_t my_write(int fd, const void *buf, size_t count);
 
 #endif  // MY_LIBASM_H

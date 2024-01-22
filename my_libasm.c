5ce49f06b524491b4ddad3860a842f6822e51c13
rebecca silverstein <silverst_r@qwasar.io>
2024-01-22 04:05:38 +0000

    chore: refactor func names and main() testing

--- my_libasm.c
+++ my_libasm.c
@@ -2,30 +2,30 @@
 #include "stdio.h"
 #include <fcntl.h>
 
-// size_t strlen(const char *str) 
+// size_t asm_strlen(const char *str) 
 // {
 //     size_t result = my_strlen(str);
 
 //     return result;
 // }
 
-// char *index(const char *s, int c) 
+// char *asm_index(const char *s, int c) 
 // {
 //     char *result = my_index(s, c);
 
 //     return result;
 // }
 
-// void *memcpy(void *dest, const void *src, size_t n) 
+// void *asm_memcpy(void *dest, const void *src, size_t n) 
 // {
 //     void *result = my_memcpy(dest, src, n);
 
 //     return result;
 // }
 
-// void *memmove(void *dest, const void *src, size_t n) 
+// void *asm_memmove(void *dest, const void *src, size_t n) 
 // {
-//     void *result = my_memcpy(dest, src, n);
+//     void *result = my_memmove(dest, src, n);
 
 //     return result;
 // }
@@ -37,28 +37,28 @@
 //     return result;
 // }
 
-// int strcasecmp(const char *s1, const char *s2) 
+// int asm_strcasecmp(const char *s1, const char *s2) 
 // {
 //     int result = my_strcasecmp(s1, s2);
 
 //     return result;
 // }
 
-// char *strchr(const char *str_1, int c) 
+// char *asm_strchr(const char *str_1, int c) 
 // {
 //     char *result = my_strchr(str_1, c);
 
 //     return result;
 // }
 
-// int strcmp(const char *str_1, const char *str_2) 
+// int asm_strcmp(const char *str_1, const char *str_2) 
 // {
 //     int result = my_strcmp(str_1, str_2);
 
 //     return result;
 // }
 
-// int strncmp(const char *s1, const char *s2, size_t n) 
+// int asm_strncmp(const char *s1, const char *s2, size_t n) 
 // {
 //     int result = my_strncmp(s1, s2, n);
 
@@ -73,12 +73,13 @@ ssize_t asm_write(int fd, const void *buf, size_t count)
 }
 
 int main() {
-    char ptr[] = "hello world!";
+    char ptr[] = "hello world! this is a test";
     char *s1 = "hello world!";
     char *s2 = "hello world";
     char *s3 = "AndNow for Something Completely DIFF";
     char c = 'w';
     size_t n = 5;
+    size_t m = 10;
     char file[] = "myfile.txt";
     int fd = open(file, O_RDWR);
 
@@ -89,6 +90,7 @@ int main() {
     // int res_strncmp = strncmp(s1, s2, n);
     // void *res_memcpy = memcpy(ptr, s3, n);
     // void *res_memset = memset(ptr, c, n);
+    // void *res_memmove = memmove(ptr + 5, ptr, m);
 
     // printf("res_strlen = %lu\n", res_strlen);
     // if (res_index) {
@@ -107,6 +109,7 @@ int main() {
     // printf("res_strncmp = %i\n", res_strncmp);
     // printf("res_memcpy = %s\n", ptr);
     // printf("res_memset = %p\n", res_memset);
+    // printf("res_memmove = %p\n", res_memmove);
 
     return 0;    
 }

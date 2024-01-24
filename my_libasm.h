4d1a781ed6e389cebe2942c6f6757e673c616799
mathius johnson <johnson_m@qwasar.io>
2024-01-24 00:13:19 +0000

    feat: complete my_strcasecmp

--- my_libasm.h
+++ my_libasm.h
@@ -10,7 +10,7 @@ extern void *my_memcpy(void *dest, const void *src, size_t n);
 extern void *my_memmove(void *dest, const void *src, size_t n);
 extern void *my_memset(void *s, int c, size_t n);
 extern ssize_t my_read(int fd, void *buf, size_t count);
-extern int my_strcasecmp(const char *s1, const char *s2);
+extern int my_strcasecmp(const char *s1, const char *s2, size_t n);
 extern char *my_strchr(const char *str_1, int c);
 extern int my_strcmp(const char *str_1, const char *str_2);
 extern int my_strncmp(const char *s1, const char *s2, size_t n);
